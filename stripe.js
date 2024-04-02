61c178e5577a2184efbe07d3b69f78e37aa2d631
Khush1694 <132439373+Khush1694@users.noreply.github.com>
2024-04-03 01:30:29 +0530

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -18,7 +18,7 @@ const user = {
 };
 
 const customer = await stripe.customers.create({
-    name: `${user.firstName} ${user.middlename} ${user.lastName}`,
+    name: `${user.firstName} ${user.middlename} ${user.lastName} saveit`,
     email: user.email,
     description: "My First Test Customer (created for API docs)",
     address: {
